3b880ba7c916d8e782655ca002ba45db63d4d42e
chen-3690 <96646758+chen-3690@users.noreply.github.com>
2022-07-10 18:20:59 +0800

    商户列表

--- src/main/java/com/example/live/mapper/MerchantMapper.java
+++ src/main/java/com/example/live/mapper/MerchantMapper.java
@@ -3,6 +3,8 @@ package com.example.live.mapper;
 import com.example.live.entity.Merchant;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 /**
  * @author baishuailei@zhejianglab.com
  * @date 2022/7/10 13:29
@@ -19,5 +21,27 @@ public interface MerchantMapper {
 
     @Update("update merchant set lt=now() where id=#{id}")
     void updateLt(@Param("id") int id);
+
+    /**
+     * 查询商户列表，包含手机、店铺名、状态条件筛选
+     * @param opeUserId
+     * @param mobile
+     * @param shop
+     * @param shopStatus
+     * @return
+     */
+    @Select({"<script>" +
+            " SELECT id,mobile,shop,shop_id,shop_status,ope_user,days,login_count,lt,ct FROM `merchant` "
+            + " <where>"
+            + " ope_user = #{opeUserId}"
+            + " <if test=' mobile != null and mobile != \"\"' > AND mobile = #{mobile}</if>"
+            + " <if test=' shop != null and shop != \"\"'> AND shop LIKE CONCAT('%',#{shop},'%')</if>"
+            + " <if test=' shopStatus != null and shopStatus != \"\" '> AND shop_status = #{shopStatus}</if>"
+            + " </where>"
+            + " </script> "})
+    List<Merchant> getMerchantListByParams(@Param("opeUserId") Integer opeUserId, @Param("mobile")String mobile, @Param("shop") String shop, @Param("shopStatus") String shopStatus);
+
+
+
 
 }
