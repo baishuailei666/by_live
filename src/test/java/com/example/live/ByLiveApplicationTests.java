5030b2268d8e57306f8a8402dc7c06f329c0108e
chen-3690 <96646758+chen-3690@users.noreply.github.com>
2022-07-25 23:42:07 +0800

    二维码生成工具

--- src/test/java/com/example/live/ByLiveApplicationTests.java
+++ src/test/java/com/example/live/ByLiveApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.live;
 
+import com.example.live.util.QRCode;
+import com.example.live.util.QRCodeUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class ByLiveApplicationTests {
 
     @Test
     void contextLoads() {
+        String base64QRCode = QRCodeUtil.getBase64QRCode("https://blog.csdn.net/su2231595742/article/details/119743585");
+        System.out.println(base64QRCode);
+        System.out.println("-------------");
+        String code = QRCode.code("www.baidu.com");
+        System.out.println(code);
     }
 
 }
