ef887d76788f5a15aa9fc59dbb47c69654c40f1a
chen-3690 <96646758+chen-3690@users.noreply.github.com>
2022-08-05 22:37:27 +0800

    合同预览

--- src/main/java/com/example/live/controller/ContractController.java
+++ src/main/java/com/example/live/controller/ContractController.java
@@ -1,10 +1,19 @@
 package com.example.live.controller;
 
 import com.example.live.common.BaseResult;
+import com.example.live.common.Constant;
 import com.example.live.controller.query.ContractQuery;
 import com.example.live.service.ContractService;
+import com.example.live.util.CloudSignUtil;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * 合同
@@ -30,67 +39,60 @@ public class ContractController {
         return contractService.contractList(query);
     }
 
-    /**
-     * 合同详情
-     *
-     * @param id document_id
-     * @return
-     */
-    @GetMapping("/info")
-    public BaseResult<?> contractInfo(@RequestParam("id") String id) {
-        return contractService.contractInfo(id);
-    }
 
-//  todo 明天搞
-//    @Autowired
-//    private CloudSignUtil cloudSignUtil;
-//
-//    /**
-//     * 合同预览
-//     * @param id
-//     * @param response
-//     */
-//    public void contractView(@RequestParam("id") String id, HttpServletResponse response) {
-//        String url = cloudSignUtil.signDown(id);
-//        long timeMillis = System.currentTimeMillis();
-//        //临时文件路径
-//        File path = Constant.getCurrentPath(Constant.TEMPORARY);
-//        FileWriter fileWriter;
-//        try {
-//            fileWriter = new FileWriter(path + Constant.separator() + timeMillis + ".pdf", false);
-//            fileWriter.write(data);
-//            fileWriter.close();
-//        } catch (IOException e) {
-//            log.error("文件输出有误。");
-//        }
-//
-//        byte[] read = null;
-//        try {
-//            read = FileUtils.readFileToByteArray(new File(path + Constant.separator() + timeMillis + ".txt"));
-//        } catch (IOException e) {
-//            log.error("文件读取有误。");
-//        }
-//        // 删除临时文件
-//        FileUtils.delete(path);
-//
-//        // 将文件内容 byte[]，通过 response 返回给客户端进行下载
-//        if (read != null && read.length > 0) {
-//            try {
-////                response.setContentType("application/octet-stream");
-//                response.setContentType("application/txt");
-//                response.setCharacterEncoding("utf-8");
-//                response.setHeader("Content-disposition", "attachment; filename=json.txt");
-//                response.setHeader("Content-Length", String.valueOf(read.length));
-//                // 这里URLEncoder.encode可以防止中文乱码
-//                String fileName = URLEncoder.encode("季度报告.txt", "UTF-8").replaceAll("\\+", "%20");
-//                response.setHeader("Content-disposition", "attachment;filename=" + fileName);
-//                response.getOutputStream().write(read);
-//                response.flushBuffer();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
+    @Autowired
+    private CloudSignUtil cloudSignUtil;
+
+
+    /**
+     * 合同预览（合同详情）
+     *
+     * @param id
+     * @param response
+     */
+    @GetMapping("/preview")
+    public void contractView(@RequestParam("id") String id, HttpServletResponse response) {
+        //获取合同下载url
+        String pdfUrl = cloudSignUtil.signDown(id);
+        long timeMillis = System.currentTimeMillis();
+        //临时文件夹路径
+        File path = Constant.getCurrentPath(Constant.TEMPORARY);
+        //文件路径
+        String filePath = path + Constant.separator() + timeMillis + ".pdf";
+        try {
+            URL url = new URL(pdfUrl);
+            File file = new File(filePath);
+            FileUtils.copyURLToFile(url, file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("保存文件出错");
+        }
+
+        //读取文件
+        byte[] read = null;
+        try {
+            read = FileUtils.readFileToByteArray(new File(filePath));
+        } catch (IOException e) {
+            System.out.println("文件读取有误");
+        }
+        // 将文件内容 byte[]，通过 response 返回给客户端进行下载
+        if (read != null && read.length > 0) {
+            try {
+                response.setContentType("application/pdf");
+                response.setHeader("Content-Length", String.valueOf(read.length));
+                response.getOutputStream().write(read);
+                response.flushBuffer();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    FileUtils.forceDelete(new File(filePath));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     /**
      * 合同下载
